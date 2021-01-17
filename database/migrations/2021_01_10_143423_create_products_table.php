2620741a44568049d80548aa97321894de5c2832
jamesnicholson <jam3s.nicholson@gmail.com>
2021-01-17 11:33:02 +0200

    Added Modals and controllers

--- database/migrations/2021_01_10_143423_create_products_table.php
+++ database/migrations/2021_01_10_143423_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,11 @@ class CreateProductTable extends Migration
      */
     public function up()
     {
-        Schema::create('product', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreign('affiliate_id')->references('id')->on('affiliates');
+            $table->foreign('link_id')->references('id')->on('links');
             $table->timestamps();
         });
     }
@@ -27,6 +29,6 @@ class CreateProductTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('product');
+        Schema::dropIfExists('products');
     }
 }
